4c1ac9fc92b2590dbb71f1b84c92d902a872e8b6
Douglas Diniz <douglasslycooper@gmail.com>
2024-02-08 16:40:17 -0300

    feat: list all users, unique user and delete account

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,6 +1,9 @@
-import { userRequestInterface, userResponseInterface } from "../interfaces/user.interfaces"
+import {  userRequestInterface, userResponseArrayInterface, userResponseInterface } from "../interfaces/user.interfaces"
 import { Request, Response } from "express"
 import { createUserService } from "../services/user/createUser.service"
+import { listAllUsersService } from "../services/user/listAllUsers.service"
+import { listUserService } from "../services/user/listUser.service"
+import { deleteOwnAccountService } from "../services/user/deleteOwnAccount.service"
 
 export const createUserController = async (req:Request, res:Response):Promise<Response> => {
         const data: userRequestInterface = req.body
@@ -8,4 +11,32 @@ export const createUserController = async (req:Request, res:Response):Promise<Re
         const newUser: userResponseInterface = await createUserService(data) 
 
         return res.status(201).json(newUser)
+}
+
+
+
+export const listAllUsersController = async (req:Request, res:Response):Promise<Response> => {
+        
+        const users: userResponseArrayInterface = await listAllUsersService()
+
+        return res.status(200).json(users)
+}
+
+
+export const listUserController = async (req:Request, res:Response):Promise<Response> => {
+        const paramUUID = req.params.uuid
+
+        const user: userResponseInterface = await listUserService(paramUUID)
+
+        return res.status(200).json(user)
+}
+
+
+export const deleteOwnAccount = async (req:Request, res:Response):Promise<Response> => {
+        const userUUID = res.locals.userUUID
+
+        await deleteOwnAccountService(userUUID)
+
+
+        return res.status(204).json()
 }
--- src/routers/user.routes.ts
+++ src/routers/user.routes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { ensureDataIsValidMiddleware } from "../middlewares/ensureDataIsValid.Middleware";
 import { userSchemaRequest } from "../schemas/user.schemas";
-import { createUserController } from "../controllers/user.controller";
+import { createUserController, deleteOwnAccount, listAllUsersController, listUserController } from "../controllers/user.controller";
 import { ensureEmailIsUniqueMiddleware } from "../middlewares/ensureEmailIsUnique.Middleware";
+import { ensureIsAuthMiddleware } from "../middlewares/ensureIsAuth.Middleware";
+import { ensureUserExistsMiddleware } from "../middlewares/ensureUserExists.Middleware";
 
 export const userRoutes:Router = Router();
 
@@ -13,3 +15,19 @@ userRoutes.post(
     ensureEmailIsUniqueMiddleware,
     createUserController
 )
+
+
+userRoutes.get("", 
+    ensureIsAuthMiddleware,
+    listAllUsersController)
+
+
+userRoutes.get("/:uuid", 
+    ensureIsAuthMiddleware,
+    ensureUserExistsMiddleware,
+    listUserController)
+
+
+userRoutes.delete("", 
+    ensureIsAuthMiddleware,
+    deleteOwnAccount)
